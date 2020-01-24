1929a4fc399975cca28e73be448db18565ed367d
jose <josemiramos99@gmail.com>
2020-01-24 17:01:39 +0100

    Update PRUEBA_1.java

--- PRUEBA_1.java
+++ PRUEBA_1.java
@@ -1,11 +1,34 @@
 
 public class PRUEBA_1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("MEGA F");
-		System.out.println();
-		System.out.println();
-	}
+
+
+		System.out.println("******Dividir en 2 comerciante compra/venta de vehiculos y admin de trabajadores******");
+		for (String string : args) {
+			
+		}
+		do {
+			System.out.println("Opciones: ");
+			System.out.println("");
+			System.out.println("1. Vender.");
+			System.out.println("");
+			System.out.println("2. Comprar.");
+			System.out.println("");
+			System.out.println("3. Reparar.");
+			System.out.println("");
+			System.out.println("4. Modificar.");
+			System.out.println("");
+			System.out.println("5. Historial.");
+			System.out.println("");
+			System.out.println("6. Agregar empleado");
+			System.out.println("");
+			System.out.println("7. Pagar a empleados");
+			
+		
+		
+		}while
+
+
+
 
 }
