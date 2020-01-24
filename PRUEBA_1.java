8de0796981b46e62863739d6ee0b061b5c00ae37
jose <josemiramos99@gmail.com>
2020-01-24 16:41:59 +0100

    Update PRUEBA_1.java

--- PRUEBA_1.java
+++ PRUEBA_1.java
@@ -4,6 +4,8 @@ public class PRUEBA_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("MEGA F");
+		System.out.println();
+		System.out.println();
 	}
 
 }
