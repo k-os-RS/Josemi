f4b2ab37927c67e30f44785a1a4798d26597f72e
Viti01g <60265664+Viti01g@users.noreply.github.com>
2020-01-24 17:06:28 +0100

    Update PRUEBA_1.java

--- PRUEBA_1.java
+++ PRUEBA_1.java
@@ -1,11 +1,19 @@
-
+import java.util.Scanner ;
 public class PRUEBA_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int oper;
+		Scanner sc=new Scanner(System.in);
 		System.out.println("MEGA F");
 		System.out.println("Super mega efote");
 		System.out.println();
+		do{
+			System.out.println("¿Que va a pasar?");
+			System.out.println("1. Brandon suspende.");
+			System.out.println("2. Brundon suspende.");
+			oper=oper.nextInt();
+		}
 	}
 
 }
