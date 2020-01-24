a17df31404108d019eebc6daf5486fea5e0fd074
Viti01g <60265664+Viti01g@users.noreply.github.com>
2020-01-24 16:58:51 +0100

    Update PRUEBA_1.java

--- PRUEBA_1.java
+++ PRUEBA_1.java
@@ -4,7 +4,7 @@ public class PRUEBA_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("MEGA F");
-		System.out.println();
+		System.out.println("Super mega efote");
 		System.out.println();
 	}
 
