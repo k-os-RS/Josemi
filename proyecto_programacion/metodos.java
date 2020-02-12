5ab961a77e64417835b167124abeab8e0df1b3cb
jose <josemiramos99@gmail.com>
2020-02-12 16:42:02 +0100

    Update metodos.java

--- proyecto_programacion/metodos.java
+++ proyecto_programacion/metodos.java
@@ -663,6 +663,7 @@ class metodos {
 		System.out.println("Coche - Moto | Si desea cancelar el alta escriba Cancelar\n");
 		System.out.print("Tipo de vehiculo: ");
 		tipoVehiculo= teclado.nextLine();
+		
 		do {
 			if (tipoVehiculo.equals("")) {
 				System.out.println("\nError: Por favor rellene el campo para poder continuar");
@@ -794,16 +795,16 @@ class metodos {
 				System.out.println("\nRellene los siguientes datos para el moto: \n");
 				//Matricula
 				do {
-					System.out.print("Matricula: ");
+					System.out.print("Matr\u00edcula: ");
 					datos= teclado.nextLine();
 
 					if (datos.equals("")) {
-						System.out.println("\nError: Por favor rellene el campo matricula");
+						System.out.println("\nError: Por favor rellene el campo matr\u00edcula");
 					} else if (isMatricula(datos)) {
 						m.setMatricula(datos);
 						correcto= true;
 					} else {
-						System.out.println("\nError: La matricula no es v\u00e1lido");
+						System.out.println("\nError: La matr\u00edcula no es v\u00e1lida");
 					}//Fin del if
 
 				} while (!correcto);
@@ -866,7 +867,7 @@ class metodos {
 							System.out.println("La cantidad de plazas debe ser mayor a 1");
 						}
 					} else {
-						System.out.println("\nError: El n\u00famero no es v\u00a1lido");
+						System.out.println("\nError: El n\u00famero no es v\u00e1lido");
 					}//Fin del if
 
 				} while (!correcto);
